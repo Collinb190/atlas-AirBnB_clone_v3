8dd9d1b5235e0546febc6c299d883a4c05ae7dbf
Collinb190 <collinb190@gmail.com>
2024-03-17 09:37:20 -0500

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script to start Flask app"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,7 +16,14 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 error"""
+    responce = {"error": "Not Found"}
+    return jsonify(responce), 404
+
+
 if __name__ == '__main__':
     HOST = getenv("HBNB_API_HOST", "0.0.0.0")
     PORT = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
